272d05dd3280f083bba088d04f0b7ce4d73c08a9
20436386 <brandon16scott@gmail.com>
2022-04-12 17:53:39 +0200

    Project initialized

---
+++ src/main/java/firstAPI/example/firstAPI/FirstApiApplication.java
@@ -0,0 +1,13 @@
+package firstAPI.example.firstAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApiApplication.class, args);
+	}
+
+}
